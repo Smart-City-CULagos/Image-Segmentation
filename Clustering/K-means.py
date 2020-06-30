b455adea5c0daf856b72e65ddd9577f35c295f4c
KawaiiCyberKnight <55341734+KawaiiCyberKnight@users.noreply.github.com>
2020-06-29 23:36:22 -0500

    Create K-means.py

---
+++ Clustering/K-means.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+K-Means
+
+@author: Christian Quintanar
+"""
+
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+
+img = Image.open("")
+img = np.asarray(img)/255
+
+img_n =img.reshape(img.shape[0]*img.shape[1], img.shape[2])
+
+kmeans = KMeans(n_clusters=5, random_state=0).fit(img_n)
+pic2show = kmeans.cluster_centers_[kmeans.labels_]
+
+cluster_pic = pic2show.reshape(img.shape[0], img.shape[1], img.shape[2])
+
+
+plt.subplot(121)
+plt.title("Original")
+plt.imshow(img, cmap="Greys_r")
+plt.subplot(122)
+plt.title("K-Means")
+plt.imshow(cluster_pic)
+plt.show()
